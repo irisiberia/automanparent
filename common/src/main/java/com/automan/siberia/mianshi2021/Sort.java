957fa777bf0e5731b8bfb822cdeb0acf2bf95ead
zhouhe <zhouhe@do-global.com>
2021-07-14 16:16:18 +0800

    java

--- common/src/main/java/com/automan/siberia/mianshi2021/Sort.java
+++ common/src/main/java/com/automan/siberia/mianshi2021/Sort.java
@@ -156,11 +156,67 @@ public class Sort {
         return head;
     }
 
+    /**
+     * 01234567
+     * <p>
+     * --->
+     * <p>
+     * 5678901234
+     */
+    public static int serch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else {
+                //mid在右边有序内
+                if (arr[mid] < arr[end]) {
+                    if (arr[mid] < target && arr[end] >= target) {
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }
+                } else {
+                    if (arr[mid] > target && arr[start] <= target) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 先递增后递减数组找到最大值
+     */
+
+    public static int serch2(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1]) {
+                start = mid + 1;
+            } else if (arr[mid] < arr[mid + 1]) {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 
     public static void main(String[] args) {
         int[] arr = new int[]{1, 3, 4, 6, 8, 9, 12, 16};
         int[] arr1 = new int[]{1, 3, 4, 6, 8, 9, 12, 16};
+
+        int[] arr2 = new int[]{5, 6, 7, 0, 1, 2, 3, 4};
         int[] merge = merge(arr, arr1);
+        System.out.println(serch(arr2, 7));
         System.out.println(binarySearch(arr, 16));
     }
 }
